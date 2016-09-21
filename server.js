1a37586a36a3e9fb666334e8fbeef45a10d4042b
shubham9730 <shubham.ghadage@gmail.com>
2016-09-21 19:04:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.send("Article one Requested will be served Here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req, res){
     res.send("Article two Requested will be served Here");
